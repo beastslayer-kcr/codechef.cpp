3d67abaa3669827cb1a436460aabe067d9f3aca2
beastslayer-kcr <61041897+beastslayer-kcr@users.noreply.github.com>
2020-10-24 15:51:02 +0530

    Create life,the universe,and everything.cpp

---
+++ life,the universe,and everything.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x<100 & x != 42){
+	    cout<<x<<endl;
+	    return main();
+	}
+	return 0;
+}
